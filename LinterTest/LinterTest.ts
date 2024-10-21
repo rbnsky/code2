903fe011c6900e54c57f2eab32380ecaceb490cf
rbnsky <robin@rofili.de>
2024-10-21 12:29:55 +0200

    complete lintertest

--- LinterTest/LinterTest.ts
+++ LinterTest/LinterTest.ts
@@ -1,29 +1,24 @@
-namespace linterTest {
+namespace LinterTest {
 
-  interface info {
+  interface Info {
     text: string;
-    key: key;
+    key: KEY;
   }
 
-  enum key {
-    Pos = 1, 
-    neg = -1
+  enum KEY {
+    POS = 1,
+    NEG = -1
   }
 
-  let info = 
-  { text: "G`udetmvhsgBncd1 ", key.Pos };
-  console.log(DeCrypt(info.text, info.key));
+  const info: Info = { text: "G`udetmvhsgBncd1 ", key: KEY.POS };
+  console.log(deCrypt(info.text, info.key));
 
-  function DeCrypt(text: string, _Key: number) 
-  {
+  function deCrypt(_text: string, _key: number): string {
     let result: string = "";
-    for (let i = 0; i < text.length(); i + +);
-      result += String.fromCharCode(text.charCodeAt(i) + _Key)
+    for (let i: number = 0; i < _text.length; i++) {
+      result += String.fromCharCode(_text.charCodeAt(i) + _key);
+    }
+
     return result;
   }
-  
-  enum key {
-    Pos = 1, neg = -1
-  }
-
 }
