4c32c2d7de40030e37edaf3150fce214345d9965
rbnsky <robin@rofili.de>
2024-10-23 15:58:45 +0200

    oldmacdonald

--- Pages/OldMacDonaldsFarm/main.js
+++ Pages/OldMacDonaldsFarm/main.js
@@ -1,62 +1,43 @@
-namespace OldMacDonaldsFarm {
-
+"use strict";
+var OldMacDonaldsFarm;
+(function (OldMacDonaldsFarm) {
     class Animal {
-        public name: string;
-        public type: string;
-        public food: string;
-        public appetite: number;
-        public sound: string;
-
-        public constructor(_name: string, _type: string, _food: string, _appetite: number, _sound: string) {
+        constructor(_name, _type, _food, _appetite, _sound) {
             this.set(_name, _type, _food, _appetite, _sound);
         }
-    
-        public set(_name: string, _type: string, _food: string, _appetite: number, _sound: string): void {
+        set(_name, _type, _food, _appetite, _sound) {
             this.name = _name;
             this.type = _type;
             this.food = _food;
             this.appetite = _appetite;
             this.sound = _sound;
         }
-
-        public eat(_type: string, _food: string, _appetite: number): void {
-
+        eat(_type, _food, _appetite) {
         }
-
-        public sing(_name: string, _sound: string): void {
-
+        sing(_name, _sound) {
         }
     }
-
     class Food {
-        type: string;
-        amount: number;
-
-        public constructor(_type: string, _amount: number) {
+        constructor(_type, _amount) {
             this.set(_type, _amount);
         }
-
-        public set(_type: string, _amount: number): void {
+        set(_type, _amount) {
             this.type = _type;
             this.amount = _amount;
         }
-
-        consumtion(_type: string, _amount: number): void {
-
+        consumtion(_type, _amount) {
         }
     }
-    
-    const animals: Array<Animal> = [];
+    const animals = [];
     animals.push(new Animal("Henriette", "Chicken", "Seeds", 56, "Gawk-gawk"));
     animals.push(new Animal("Kermit", "Frog", "Flies", 10, "Ribbit"));
     animals.push(new Animal("Pferdinand", "Horse", "Apples", 12, "Hüüü"));
     animals.push(new Animal("Sneaky", "Fox", "Rabbits", 1, "Hati-hati-hati-how"));
-    animals.push(new Animal("Frieda", "Cat", "Fish", 2, "Meow"))
-
-    const stockpile: Array<Food> = [];
+    animals.push(new Animal("Frieda", "Cat", "Fish", 2, "Meow"));
+    const stockpile = [];
     stockpile.push(new Food("Seeds", 1032));
     stockpile.push(new Food("Flies", 137));
     stockpile.push(new Food("Apples", 28));
     stockpile.push(new Food("Rabbits", 5));
     stockpile.push(new Food("Fish", 9));
-}
+})(OldMacDonaldsFarm || (OldMacDonaldsFarm = {}));
