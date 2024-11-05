ec0fde2e7cfd49e8d8f4c74f732c3d705402297e
rbnsky <robin@rofili.de>
2024-11-05 16:26:11 +0100

    ahhhhh

--- Fudge/FirstFudge/FirstFudge.js
+++ Fudge/FirstFudge/FirstFudge.js
@@ -3,18 +3,20 @@ var FirstFudge;
 (function (FirstFudge) {
     var f = FudgeCore;
     window.addEventListener("load", start);
-    f.loop;
+    // create a node as the scene graph
+    const node = new f.Node("Node");
+    console.log(node);
+    let globalViewport;
+    f.Loop.addEventListener("loopFrame" /* f.EVENT.LOOP_FRAME */, rotateCube);
+    f.Loop.start;
     function start(_event) {
         const canvas = document.querySelector("canvas");
         console.log(canvas);
-        // create a node as the scene graph
-        const node = new f.Node("Node"); // create a node as the scene graph
-        console.log(node);
         // create MeshCube
         const cube = new f.MeshCube("Cube");
-        // refer the mesh to the node
+        // refer MesH´hCube to the node
         node.addComponent(new f.ComponentMesh(cube));
-        // rotate the mesh
+        // rotate meshCube (once)
         node.getComponent(f.ComponentMesh).mtxPivot.rotate(new f.Vector3(-30, 30, -50));
         console.log(cube);
         // create a material with the default texture for testing (I replaced it with a color)
@@ -24,26 +26,35 @@ var FirstFudge;
         console.log(material);
         //create ComponentMaterial
         const cmpmaterial = new f.ComponentMaterial(material);
-        // set ComponentMaterial color (r, g, b, alpha) - all 0-1
+        // set ComponentMaterial color (r, g, b, alpha) - all 0-1 (alpha doesnt work if material is lit)
         cmpmaterial.clrPrimary.set(1, 0.4, 0.7, 1);
         // refer ComponentMaterial to the node
         node.addComponent(cmpmaterial);
         console.log(cmpmaterial);
         // create a camera
-        const camera = new f.ComponentCamera;
+        const camera = new f.ComponentCamera();
         console.log(camera);
         // move the camera
-        camera.mtxPivot.translateZ(+5);
-        //rotate the camera
-        camera.mtxPivot.rotateY(180);
+        camera.mtxPivot.translateZ(-3);
+        // rotate the camera (only if we look away from the camera)
+        //camera.mtxPivot.rotateY(180);
         // create a viewport to manage rendering of the graph to the canvas via the camera
         const viewport = new f.Viewport();
         viewport.initialize("Viewport", node, camera, canvas);
-        const cmptransform = new f.ComponentTransform;
+        viewport.draw();
+        globalViewport = viewport;
+        //const cmptransform: f.ComponentTransform = new f.ComponentTransform;
         //node.addComponent(componenttransform);
         //node.getComponent(f.ComponentTransform).mtxLocal.rotateY(1);
+        //node.mtxLocal.rotateY(1);
+        f.Loop.start();
+        f.Time.game.setScale(1);
+    }
+    function rotateCube() {
+        const frameTimeInMilliSeconds = f.Loop.timeFrameGame;
+        const frameTimeInSeconds = (frameTimeInMilliSeconds / 1000);
+        const degrees = 360 / frameTimeInMilliSeconds;
         node.mtxLocal.rotateY(1);
-        // render
-        viewport.draw();
+        globalViewport.draw();
     }
 })(FirstFudge || (FirstFudge = {}));
--- Fudge/FirstFudge/FirstFudge.ts
+++ Fudge/FirstFudge/FirstFudge.ts
@@ -3,26 +3,29 @@ namespace FirstFudge {
 
     window.addEventListener("load", start);
 
+    // create a node as the scene graph
+    const node: f.Node = new f.Node("Node");
+
+    console.log(node);
+
+    let globalViewport: f.Viewport;
+
     f.Loop.addEventListener(f.EVENT.LOOP_FRAME, rotateCube);
-    f.Loop.start
+    f.Loop.start;
 
     function start(_event: Event): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
         console.log(canvas);
 
-        // create a node as the scene graph
-        const node: f.Node = new f.Node("Node"); // create a node as the scene graph
-
-        console.log(node);
-
         // create MeshCube
         const cube: f.Mesh = new f.MeshCube("Cube");
-        // refer the mesh to the node
-        node.addComponent(new f.ComponentMesh(cube))
-        // rotate the mesh
+        // refer MesH´hCube to the node
+        node.addComponent(new f.ComponentMesh(cube));
+        // rotate meshCube (once)
         node.getComponent(f.ComponentMesh).mtxPivot.rotate(new f.Vector3(-30, 30, -50));
 
         console.log(cube);
+
 
         // create a material with the default texture for testing (I replaced it with a color)
         //const material = new f.Material("Texture", FudgeCore.ShaderLitTextured);
@@ -30,42 +33,52 @@ namespace FirstFudge {
         // create lit material
         const material = new f.Material("Texture", f.ShaderLit);
 
-        console.log(material)
+        console.log(material);
+
 
         //create ComponentMaterial
         const cmpmaterial: f.ComponentMaterial = new f.ComponentMaterial(material);
-        // set ComponentMaterial color (r, g, b, alpha) - all 0-1
-        cmpmaterial.clrPrimary.set(1, 0.4, 0.7, 1)
+        // set ComponentMaterial color (r, g, b, alpha) - all 0-1 (alpha doesnt work if material is lit)
+        cmpmaterial.clrPrimary.set(1, 0.4, 0.7, 1);
         // refer ComponentMaterial to the node
-        node.addComponent(cmpmaterial)
+        node.addComponent(cmpmaterial);
 
-        console.log(cmpmaterial)
+        console.log(cmpmaterial);
+
 
         // create a camera
-        const camera: f.ComponentCamera = new f.ComponentCamera
+        const camera: f.ComponentCamera = new f.ComponentCamera();
+
         console.log(camera);
+
+
         // move the camera
-        camera.mtxPivot.translateZ(+5);
-        //rotate the camera
-        camera.mtxPivot.rotateY(180);
+        camera.mtxPivot.translateZ(-3);
+        // rotate the camera (only if we look away from the camera)
+        //camera.mtxPivot.rotateY(180);
+
+
         // create a viewport to manage rendering of the graph to the canvas via the camera
         const viewport: f.Viewport = new f.Viewport();
         viewport.initialize("Viewport", node, camera, canvas);
+        viewport.draw();
+        globalViewport = viewport;
 
-        const cmptransform: f.ComponentTransform = new f.ComponentTransform
+
+        //const cmptransform: f.ComponentTransform = new f.ComponentTransform;
         //node.addComponent(componenttransform);
         //node.getComponent(f.ComponentTransform).mtxLocal.rotateY(1);
+        //node.mtxLocal.rotateY(1);
+
+        f.Loop.start();
+        f.Time.game.setScale(1);
+    }
+
+    function rotateCube(): void {
+        const frameTimeInMilliSeconds: number = f.Loop.timeFrameGame;
+        const frameTimeInSeconds: number = (frameTimeInMilliSeconds / 1000);
+        const degrees: number = 360 / frameTimeInMilliSeconds;
         node.mtxLocal.rotateY(1);
-
-        function rotateCube(): void {
-            node.mtxLocal.rotateY
-
-
-
-        }
-
-        // render
-        viewport.draw();
+        globalViewport.draw();
     }
 }
-
